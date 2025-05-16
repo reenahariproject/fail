2f0ab4bb8511fe20bc82cfbef103baa0c83e9de7
reenahariproject <harivip147@gmail.com>
2025-05-16 15:08:02 +0530

    Initial commit

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,28 +1,11 @@
 import React, { useEffect } from 'react';
-import myVideo from '../assets/img/video1.mp4';
 import '../App.css';
 
 const VideoBackground = () => {
-
-  useEffect(() => {
-    const video = document.querySelector('.video-background video');
-    const handleCanPlayThrough = () => {
-      video.classList.add('ready'); 
-    };
-
-    video.addEventListener('canplaythrough', handleCanPlayThrough);
-
-    
-    return () => {
-      video.removeEventListener('canplaythrough', handleCanPlayThrough);
-    };
-  }, []); 
-
+  // You can remove useEffect if it's only related to video loading
   return (
     <div className="video-background">
-      <video autoPlay muted loop className="video" id="background-video">
-        <source src={''} type="video/mp4" />
-      </video>
+      {/* No video */}
     </div>
   );
 };
